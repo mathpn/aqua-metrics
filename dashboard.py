0d4c10545245d7eb1acd16e21f429703d0aaab53
Matheus Pedroni <pnmatheus@protonmail.com>
2024-01-12 22:22:22 -0300

    amosphetic temperature plot

--- dashboard.py
+++ dashboard.py
@@ -3,23 +3,45 @@ import sqlite3
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.title("Buoy data dashboard")
 
 
 def load_data():
     conn = sqlite3.connect("file:data/database.db?mode=ro", uri=True)
-    cur = conn.execute(
-        "SELECT ATMP FROM realtime_data WHERE ingestion_ts = (SELECT MAX(ingestion_ts) FROM realtime_data)"
+    df = pd.read_sql_query(
+        """
+        SELECT LAT, LON, ATMP
+        FROM realtime_data
+        WHERE ingestion_ts = (SELECT MAX(ingestion_ts) FROM realtime_data)
+        AND ATMP IS NOT NULL
+        """,
+        conn,
     )
-    data = (x[0] for x in cur)
-    return [x for x in data if x is not None]
+    return df
 
 
-data = load_data()
+df = load_data()
 
-st.subheader("Distribution of atmospheric temperature")
+print(df.sample(10))
 
-hist_values = np.histogram(data, bins=20)[0]
+st.subheader("Atmospheric temperature")
 
-st.bar_chart(hist_values)
+df["size"] = 10
+fig = px.scatter_mapbox(
+    df,
+    lat="LAT",
+    lon="LON",
+    color="ATMP",
+    opacity=0.5,
+    color_continuous_scale="viridis",
+    size="size",
+    zoom=1,
+)
+
+fig.update_layout(mapbox_style="carto-darkmatter", mapbox_center_lon=0)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+st.plotly_chart(fig)
