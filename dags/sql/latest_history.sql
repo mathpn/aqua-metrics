23f30da1f044ed72798c852440bede090b5a816e
Matheus Pedroni <pnmatheus@protonmail.com>
2024-01-22 22:08:54 -0300

    fix latest_history query
    find latest ingestion timestamp per station_code

--- dags/sql/latest_history.sql
+++ dags/sql/latest_history.sql
@@ -3,7 +3,11 @@ DROP TABLE IF EXISTS latest_history;
 CREATE TABLE latest_history AS
 SELECT
     *
-FROM history_data
-WHERE ingestion_ts = (
-    SELECT MAX(ingestion_ts) FROM history_data
-);
+FROM history_data AS t1
+JOIN (
+    SELECT station_code, MAX(ingestion_ts) AS ingestion_ts
+    FROM history_data
+    GROUP BY 1
+) AS t2
+ON t1.station_code = t2.station_code
+AND t1.ingestion_ts = t2.ingestion_ts;
